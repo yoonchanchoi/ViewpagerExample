001a06d86035f850237c2d02b5e7390caccf5eaf
Choi YoonChan <choyc9239@gmail.com>
2022-03-08 04:01:52 +0900

    Initial commit

---
+++ app/src/main/java/com/example/viewpagerexample/ViewPagerAdapter.java
@@ -0,0 +1,51 @@
+package com.example.viewpagerexample;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+public class ViewPagerAdapter extends FragmentPagerAdapter {
+    public ViewPagerAdapter(@NonNull FragmentManager fm) {
+        super(fm);
+    }
+
+    // 프래그먼트 교채를 보여주는 처리를 구하는 곳
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        switch (position){
+            case 0:
+                return FragMonday.newInstance();
+            case 1:
+                return FragTuesday.newInstance();
+            case 2:
+                return FragWednesday.newInstance();
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return 3;
+    }
+
+
+    //상단의 탭 레이아웃 인디케이터 쪽에 텍스트를 선언해주는 곳
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        switch (position){
+            case 0:
+                return "Monday";
+            case 1:
+                return "Tuesday";
+            case 2:
+                return "Wednesday";
+            default:
+                return null;
+        }
+    }
+}
